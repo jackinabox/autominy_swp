32a3b8dc3173786daa0da3b02c1d8775b113cfdc
jackinabox <adrian.defer@gamil.com>
2019-06-22 00:16:12 +0200

    update

---
+++ catkin_ws_swp/src/cmake-build-debug/catkin_generated/setup_cached.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/adrian/autominy_swp/catkin_ws_swp/src/devel:$CMAKE_PREFIX_PATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/adrian/autominy_swp/catkin_ws_swp/src/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/adrian/autominy_swp/catkin_ws_swp/src:$ROS_PACKAGE_PATH"
